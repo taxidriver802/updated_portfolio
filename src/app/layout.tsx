fa6a811c6ce4b0b5ea03a5ced0f8e37561bf2448
jason cox <JAcox12@icloud.com>
2026-03-13 12:52:22 -0500

    update metaData

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,10 @@ import '../styles/globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
+import { Analytics } from '@vercel/analytics/next';
+
 export const metadata: Metadata = {
+  metadataBase: new URL('https://jasoncox,dev'),
   title: 'Jason Cox | Full-Stack Developer',
   description:
     'Portfolio of Jason Cox, a full-stack developer building modern web applications with React, Node.js, and related technologies.',
@@ -12,7 +15,9 @@ export const metadata: Metadata = {
   },
   openGraph: {
     title: 'Jason Cox Portfolio',
-    images: ['/favicon1.png'],
+    description: 'Welcome to My Portfolio',
+    images: [{ url: '/favicon1.png' }],
+    url: 'https://jasoncox.dev',
   },
 };
 
@@ -22,12 +27,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="bg-white text-zinc-950 antialiased dark:bg-zinc-950 dark:text-zinc-50">
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <>
+      <html lang="en">
+        <body className="bg-white text-zinc-950 antialiased dark:bg-zinc-950 dark:text-zinc-50">
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+      <Analytics />
+    </>
   );
 }
